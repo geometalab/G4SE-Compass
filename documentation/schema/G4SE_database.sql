608e398c2d6c9f5c85d532d0a63c0d547ac2e2da
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2016-06-02 13:17:28 +0200

    Added base link

--- documentation/schema/G4SE_database.sql
+++ documentation/schema/G4SE_database.sql
@@ -8,20 +8,25 @@ CREATE TABLE public.records
   publicationyear integer NOT NULL,
   publicationlineage character varying(255),
   geography character varying(255) NOT NULL,
-  geodata_type character varying(20),
   extent box2d,
+  geodata_type character varying(255) NOT NULL,
   source character varying(2083) NOT NULL,
-  metadata character varying(2083) NOT NULL,
-  access character varying(2083) NOT NULL,
+  metadata_link character varying(2083) NOT NULL,
+  access_link character varying(2083) NOT NULL,
+  base_link character varying(2083),
   collection character varying(255),
   dataset character varying(255),
-  service character varying(20) NOT NULL,
+  arcgis_layer_link character varying(2083),
+  qgis_layer_link character varying(2083),
+  arcgis_symbology_link character varying(2083),
+  qgis_symbology_link character varying(2083),
+  service_type character varying(255),
   crs character varying(20) NOT NULL,
-  terms character varying(2083) NOT NULL,
+  term_link character varying(2083) NOT NULL,
   proved date,
+  visibility character varying(255) NOT NULL,
   modified timestamp without time zone,
-  login_name character varying(50),
-  visibility character varying(20) NOT NULL
+  login_name character varying(255)
 );
 
 CREATE INDEX record_index
