317ada28ae596d349b8255f26cce0087817fdf27
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2016-06-23 09:07:14 +0200

    Added harvesting script

---
+++ compass-harvester/src/harvest_records.py
@@ -0,0 +1,82 @@
+import os
+import sys
+import xml.etree.ElementTree as ET
+
+from sqlalchemy import create_engine, Column, Integer, String, Text, Date, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import settings
+
+Base = declarative_base()
+
+
+class Record(Base):
+    __tablename__ = "records"
+
+    identifier = Column(String, primary_key=True)
+    language = Column(String)
+    content = Column(String)
+    abstract = Column(Text)
+    publication_year = Column(Integer)
+    publication_lineage = Column(String)
+    geography = Column(String)
+    extent = Column(String)
+    geodata_type = Column(String)
+    source = Column(String)
+    metadata_link = Column(String)
+    access_link = Column(String)
+    base_link = Column(String)
+    collection = Column(String)
+    dataset = Column(String)
+    arcgis_layer_link = Column(String)
+    qgis_layer_link = Column(String)
+    arcgis_symbology_link = Column(String)
+    qgis_symbology_link = Column(String)
+    service_type = Column(String)
+    crs = Column(String)
+    term_link = Column(String)
+    proved = Column(Date)
+    visibility = Column(String)
+    modified = Column(TIMESTAMP)
+    login_name = Column(String)
+
+
+def read_records(target_dir):
+    records = []
+    for fn in os.listdir(target_dir):
+        tree = ET.parse(target_dir + '/' + fn)
+        root = tree.getroot()
+        record = {}
+        for child in root:
+            content = child.text
+            tag = child.tag.split('}', 1)[1]
+            record[tag] = content
+        records.append(record)
+    return records
+
+
+def insert_records_into_db(records):
+    engine = db_connect()
+    session = sessionmaker()
+    session.configure(bind=engine)
+
+    for record in records:
+        db_record = Record(**record)
+        s = session()
+        s.add(db_record)
+        s.commit()
+
+
+def db_connect():
+    return create_engine('postgresql://' + settings.DATABASE_URL)
+
+
+def harvest():
+    os.system('oai-harvest all')
+    target_dir = sys.argv[1]
+    record_list = read_records(target_dir)
+    insert_records_into_db(record_list)
+
+if __name__ == "__main__":
+    sys.exit(harvest())
