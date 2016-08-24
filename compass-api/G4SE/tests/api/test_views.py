828f6b5a5ba2e69c5d20bfb851c44e5e91578f9e
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2016-08-24 14:07:48 +0200

    Added a lot more testdata

--- compass-api/G4SE/tests/api/test_views.py
+++ compass-api/G4SE/tests/api/test_views.py
@@ -11,26 +11,25 @@ class TestListViews(APITestCase):
 
     def test_record_list(self):
         record_list = self.client.get('/api/')
-        assert 'Schweiz' in record_list.data[0].values()
-        assert 'Zürich' in record_list.data[2].values()
+        assert len(record_list.data) == 21
 
     def test_external_access(self):
         record_list = self.client.get('/api/', REMOTE_ADDR="123.1.1.3")
-        assert len(record_list.data) == 2
+        assert len(record_list.data) == 20
         all_record_list = self.client.get('/api/')
-        assert len(all_record_list.data) == 3
+        assert len(all_record_list.data) == 21
 
     def test_internal_record_detail(self):
-        record = self.client.get('/api/5e9c6175-ead7-4637-af93-ec28a9d28f8a/')
-        assert 'Schweiz' in record.data.values()
+        record = self.client.get('/api/5e98dd86-e3c7-4a8b-895f-d555dde45566/')
+        assert 'swissimage' in record.data.values()
 
     def test_unauthorized_detail(self):
-        record = self.client.get('/api/5e9c6175-ead7-4637-af93-ec28a9d28f8a/', REMOTE_ADDR="123.1.1.3")
+        record = self.client.get('/api/5e98dd86-e3c7-4a8b-895f-d555dde45566/', REMOTE_ADDR="123.1.1.3")
         assert record.status_code == 403
 
     def test_harvested_record_detail(self):
-        record = self.client.get('/api/dcb9cfe2-4568-4c30-971f-a4103d6e0ee9/')
-        assert 'Zürich' in record.data.values()
+        record = self.client.get('/api/17f17742-50d4-4882-94fb-3855922b0acc/')
+        assert 'GeoVITe' in record.data.values()
 
     def test_most_recent_records(self):
         record_list = self.client.get('/api/recent/?count=2')
@@ -58,10 +57,10 @@ class TestSearchViews(APITestCase):
             assert 'Zürich' in result.values()
 
     def test_external_search(self):
-        result = self.client.get('/api/search/?query=Landschaftsmodell&language=de')
-        assert len(result.data) == 1
-        empty_result = self.client.get('/api/search/?query=Landschaftsmodell', REMOTE_ADDR="123.1.1.3")
-        assert len(empty_result.data) == 0
+        result = self.client.get('/api/search/?query=swissimage&language=de')
+        assert len(result.data) == 4
+        empty_result = self.client.get('/api/search/?query=swissimage', REMOTE_ADDR="123.1.1.3")
+        assert len(empty_result.data) == 3
 
 
 @pytest.mark.django_db(transaction=True)
@@ -81,7 +80,7 @@ class TestBackendViews(APITestCase):
     def test_internal_records_list(self):
         self.client.login(username='admin', password='securepassword')
         result_list = self.client.get('/api/admin/')
-        assert len(result_list.data) == 2
+        assert len(result_list.data) == 10
 
     def test_insert_record(self):
         records_before_insert = AllRecords.objects.all().count()
