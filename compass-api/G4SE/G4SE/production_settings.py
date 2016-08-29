0449d95c5522c7deacc7534719602a8e69c092f4
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2016-08-29 09:30:59 +0200

    Added CORS support

--- compass-api/G4SE/G4SE/production_settings.py
+++ compass-api/G4SE/G4SE/production_settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
+    'corsheaders',
     'rest_framework',
     'api',
 ]
@@ -63,12 +65,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'G4SE.urls'
 
