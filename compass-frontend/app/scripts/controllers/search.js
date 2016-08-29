291b51764c351bfb728303aefdbd57bba580801d
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2016-08-29 16:43:27 +0200

    Added basic search

--- compass-frontend/app/scripts/controllers/search.js
+++ compass-frontend/app/scripts/controllers/search.js
@@ -7,7 +7,25 @@
  * # SearchCtrl
  * Controller of the g4seApp
  */
-angular.module('g4seApp')
-  .controller('SearchCtrl', function () {
+angular.module('g4seApp').service('dataService', ['$http', function ($http) {
+  return {
+    getSearchResult: function (search_query) {
+      return $http.get('http://localhost/api/search/?query=' + search_query);
+    }
+  }
+}]);
 
-  });
+
+angular.module('g4seApp')
+  .controller('SearchCtrl',['$scope', '$http', 'dataService', '$timeout', function($scope, $http, dataService, $timeout) {
+    $scope.enter_search = function() {
+      if ($scope.text){
+        dataService.getSearchResult($scope.text).then(function (result) {
+          $scope.records = result.data;
+          $timeout(function () {
+            $scope.$apply()
+          })
+        });
+      }
+    };
+  }]);
