c575b46a14ac2c23c130084c3f09be90fd4a838f
Dennis Ligtenberg <dennis_ligtenberg@hotmail.com>
2016-08-31 11:32:43 +0200

    Added single result view

--- compass-frontend/app/scripts/controllers/search.js
+++ compass-frontend/app/scripts/controllers/search.js
@@ -12,6 +12,9 @@ angular.module('g4seApp').service('dataService', ['$http', function ($http) {
     getSearchResult: function (searchQuery) {
       return $http.get('http://localhost/api/search/?query=' + searchQuery);
     },
+    getSingleResult: function (id) {
+      return $http.get('http://localhost/api/metadata/' + id);
+    },
     getRecentlyUpdated: function () {
       return $http.get('http://localhost/api/recent/?count=5');
     }
@@ -27,6 +30,16 @@ angular.module('g4seApp')
     dataService.getRecentlyUpdated().then(function (result) {
       $scope.recentRecords = result.data;
     });
+
+    $scope.singleResult = function(api_id) {
+      dataService.getSingleResult(api_id).then(function (result) {
+        $scope.records = [result.data];
+        $scope.resultCount = 1;
+        $timeout(function () {
+          $scope.$apply();
+        });
+      });
+    };
 
     $scope.enterSearch = function() {
       if ($scope.text){
