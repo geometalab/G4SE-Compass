1f8d480b7df2d4b6d353e893d57cdbc0d615f16d
Nicola Jordan <nic@hixi.ch>
2016-09-18 09:52:40 +0200

    perform an abbreviated filter for tag

--- compass-api/G4SE/api/signals.py
+++ compass-api/G4SE/api/signals.py
@@ -25,30 +25,29 @@ def _combined_query(main_tag, tags, language):
     return q
 
 
+def _search_vector(tag):
+    return {
+        Record.GERMAN: Q(search_vector_de=_combined_query(tag.tag_de, tag.tag_alternatives_de, 'german')),
+        Record.ENGLISH: Q(search_vector_en=_combined_query(tag.tag_en, tag.tag_alternatives_en, 'english')),
+        Record.FRENCH: Q(search_vector_fr=_combined_query(tag.tag_fr, tag.tag_alternatives_fr, 'french')),
+    }
+
+
 def _taggable_records(tag):
-    search_de = Q(search_vector_de=_combined_query(tag.tag_de, tag.tag_alternatives_de, 'german'))
-    search_en = Q(search_vector_en=_combined_query(tag.tag_en, tag.tag_alternatives_en, 'english'))
-    search_fr = Q(search_vector_fr=_combined_query(tag.tag_fr, tag.tag_alternatives_fr, 'french'))
+    search_vector = _search_vector(tag)
+    search_de, search_en, search_fr = search_vector[Record.GERMAN], search_vector[Record.ENGLISH], search_vector[Record.FRENCH]
     return Record.objects.filter(search_de | search_en | search_fr)
 
 
 def _is_tag_in_record(tag, record):
-    vector = {
-        Record.GERMAN: record.search_vector_de,
-        Record.ENGLISH: record.search_vector_en,
-        Record.FRENCH: record.search_vector_fr,
-    }
-    return vector[record.language].find(tag) > 0
+    search_vector = _search_vector(tag)[record.language]
+    return Record.objects.filter(api_id=record.api_id).filter(search_vector).count() > 0
+
 
 def _tags_for_record(record):
     tags = []
     for tag in RecordTag.objects.all():
-        language_tags = {
-            Record.GERMAN: [tag.tag_de] + tag.tag_alternatives_de,
-            Record.ENGLISH: [tag.tag_en] + tag.tag_alternatives_en,
-            Record.FRENCH: [tag.tag_fr] + tag.tag_alternatives_fr,
-        }
-        if any(_is_tag_in_record(tag_word.lower(), record) for tag_word in language_tags[record.language]):
+        if _is_tag_in_record(tag, record):
             tags.append(tag)
     return list(set(tags))
 
