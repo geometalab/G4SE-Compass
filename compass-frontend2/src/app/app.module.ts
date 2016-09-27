9fbd416de62c5db23c8634e415c489aabb453b69
Nicola Jordan <njordan.hsr@gmail.com>
2016-09-27 08:56:47 +0200

    base foundation for anglar2 version

---
+++ compass-frontend2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MetadataComponent } from './metadata/metadata.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MetadataComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ compass-frontend2/src/app/metadata/metadata.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { MetadataComponent } from './metadata.component';
+
+describe('Component: Metadata', () => {
+  it('should create an instance', () => {
+    let component = new MetadataComponent();
+    expect(component).toBeTruthy();
+  });
+});
