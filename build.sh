3de41643ed7ed0ae9314cef6f19baa1a75467f3d
Nicola Jordan <njordan.hsr@gmail.com>
2016-09-29 11:51:06 +0200

    use new frontend

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-cd compass-frontend
-grunt clean
-grunt build
-cd ..
-rm -rf nginx/g4se-frontend
-cp -r compass-frontend/dist nginx/g4se-frontend
+cd compass-frontend2
+rm -rf dist
+ng build -prod
 docker-compose build
