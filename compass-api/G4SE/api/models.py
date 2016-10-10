07a5af128da2eb80cc0f1318cbebe353585239fc
Nicola Jordan <njordan.hsr@gmail.com>
2016-10-10 14:52:49 +0200

    add new table

--- compass-api/G4SE/api/models.py
+++ compass-api/G4SE/api/models.py
@@ -202,3 +202,105 @@ def record_ids_for_search_query(search_query, language='de'):
     return RecordTaggedItem.objects.filter(
         record_tag__in=tags
     ).values_list('object_id', flat=True)
+
+
+class GeoServiceMetadata(models.Model):
+    """
+    G4SE Geografic Services' metadata
+    """
+    GERMAN = 'de'
+    FRENCH = 'fr'
+    ENGLISH = 'en'
+
+    LANGUAGE_CHOICES = (
+        (GERMAN, _('de')),
+        (FRENCH, _('fr')),
+        (ENGLISH, _('en')),
+    )
+
+    VISIBILITY_PUBLIC = 'public'
+    VISIBILITY_TEST = 'test'
+    VISIBILITY_HSR_INTERNAL = 'hsr-internal'
+    VISIBILITY_CHOICES = (
+        (VISIBILITY_PUBLIC, _('public')),
+        (VISIBILITY_TEST, _('test')),
+        (VISIBILITY_HSR_INTERNAL, _('hsr-internal')),
+    )
+
+    GEO_DATA_TYPE_RASTER = 'raster'
+    GEO_DATA_TYPE_VECTOR = 'vector'
+    GEO_DATA_TYPES = (
+        (GEO_DATA_TYPE_RASTER, _('raster')),
+        (GEO_DATA_TYPE_VECTOR, _('vector')),
+    )
+
+    api_id = models.UUIDField(_('uuid'), primary_key=True, editable=False, default=uuid.uuid4)
+    identifier = models.CharField(_('external identifier'), max_length=255)
+    language = models.CharField(_('language'), max_length=20, choices=LANGUAGE_CHOICES, default=GERMAN)
+    title = models.CharField(_('title'), max_length=255)
+    abstract = models.TextField(_('abstract'))
+    publication_year = models.IntegerField(_('publication year'), max_length=4)
+    publication_lineage = models.CharField(_('history'), max_length=255, blank=True, null=True)
+    is_latest = models.BooleanField(_('latest of series'), max_length=255, default=False)
+    geography = models.CharField(_('geography'), max_length=255, default='Schweiz')
+    extent = models.CharField(
+        _('extent'), max_length=255, null=True, blank=True, help_text='needs follow the form `BOX(x1 y1,x2 y2)`'
+    )
+    geodata_type = models.CharField(_('geodata type'), max_length=255, choices=GEO_DATA_TYPES)
+    source = models.CharField(_('source'), max_length=2083)
+    metadata_link = models.URLField(_(''), max_length=2083)
+    access_link = models.URLField(_(''), max_length=2083)
+    base_link = models.URLField(_('access to data'), max_length=2083, blank=True, null=True)
+    collection = models.CharField(_('group name'), max_length=255, blank=True, null=True)
+    dataset = models.CharField(_('dataset name'), max_length=255, blank=True, null=True)
+    arcgis_layer_link = models.URLField(_('ArcGIS layer link'), max_length=2083, blank=True, null=True)
+    qgis_layer_link = models.URLField(_('QGIS layer link'), max_length=2083, blank=True, null=True)
+    arcgis_symbology_link = models.URLField(_('ArcGIS symbology link'), max_length=2083, blank=True, null=True)
+    qgis_symbology_link = models.URLField(_('QGIS symbology link'), max_length=2083, blank=True, null=True)
+    service_type = models.CharField(_('service type'), max_length=255, blank=True, null=True)
+    crs = models.CharField(_('coordinate reference system'), max_length=20)
+    term_link = models.URLField(_('terms of use'), max_length=2083)
+    proved = models.DateField(_('proving date'), blank=True, null=True)
+    visibility = models.CharField(
+        _('access restriction'), max_length=255, choices=VISIBILITY_CHOICES, default=VISIBILITY_PUBLIC
+    )
+    login_name = models.CharField(_('login name'), max_length=255)
+
+    modified = models.DateTimeField(_('last modification'), auto_now=True, blank=True, null=True)
+    created = models.DateTimeField(_('created on'), auto_now_add=True)
+    imported = models.BooleanField(_('imported'), editable=False, default=False)
+
+    @property
+    def tags(self):
+        return RecordTaggedItem.objects.filter(object_id=self.api_id).order_by('id')
+
+    @property
+    def tags_de(self):
+        tags = []
+        for tag in self.tags:
+            tags += tag.all_tag_list(self.GERMAN)
+        return tags
+
+    @property
+    def tags_en(self):
+        tags = []
+        for tag in self.tags:
+            tags += tag.all_tag_list(self.ENGLISH)
+        return tags
+
+    @property
+    def tags_fr(self):
+        tags = []
+        for tag in RecordTaggedItem.objects.filter(object_id=self.api_id).order_by('id'):
+            tags += tag.all_tag_list(self.FRENCH)
+        return tags
+
+    def tag_list_display(self):
+        tags = self.tags
+        if len(tags) > 0:
+            return ', '.join([str(t) for t in tags])
+        return ' - '
+    tag_list_display.short_description = 'tags'
+
+    def __str__(self):
+        return self.content
