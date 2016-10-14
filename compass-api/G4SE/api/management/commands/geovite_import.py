48335b66bc5f4f1c43faedc5e3b5a9bcdca9bc13
Nicola Jordan <nic@hixi.ch>
2016-10-14 12:11:54 +0200

    rename importer more properly

--- compass-api/G4SE/api/management/commands/geovite_import.py
+++ compass-api/G4SE/api/management/commands/geovite_import.py
@@ -4,7 +4,7 @@ from api.management.commands._private import harvest
 
 
 class Command(BaseCommand):
-    help = 'Updates all GEOVITE imports (HarvestedRecords)'
+    help = 'Updates all GeoVITe imports (HarvestedRecords)'
 
     def handle(self, *args, **options):
         harvest()
