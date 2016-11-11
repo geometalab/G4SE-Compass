869aea9590b1e37526d86fc39a67780cb3b20e11
Nicola Jordan <njordan.hsr@gmail.com>
2016-11-11 10:15:06 +0100

    update index after reindexing tags

--- compass-api/docker-entrypoint.sh
+++ compass-api/docker-entrypoint.sh
@@ -7,4 +7,5 @@ python3 manage.py collectstatic --noinput
 /bin/wait-for-it.sh -t 30 elasticsearch:9200
 python3 manage.py rebuild_index --noinput
 python3 manage.py retag_records
+python3 manage.py update_index
 exec "$@"
