bc11e65be9cd70e395d991ae248bdb639629beab
Nicola Jordan <nic@hixi.ch>
2017-03-30 13:07:00 +0200

    change title

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -5,7 +5,7 @@
         <router-link to="/search"><img width="100px" src="../assets/logo.jpg"></router-link>
       </div>
       <div class="col-5">
-        <h3>&nbsp;G4SE Geodata</h3>
+        <h3> HSR Geodatenkompass</h3>
       </div>
       <div class="col-3">
         <label for="search-language">search language</label>
